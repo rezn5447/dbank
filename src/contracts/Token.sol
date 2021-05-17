104cc5cb0166e742e12345eed82f456d600bb47d
Resdan Logan <rezn5447@gmail.com>
2021-05-17 10:24:06 -0700

    WIP 22 mins

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+  string public name = "MyName";
   //add minter variable
+  address public minter;
 
   //add minter changed event
 
   constructor() public payable ERC20("Name", "Symbol") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
